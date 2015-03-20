8cbb9f8fc2ddbee3f833cc8ebd0ff9bb7bcaa335
datahobo <matthew.wakeman@gmail.com>
2015-03-20 13:42:20 -0400

    starting graphics

--- lego-pricing/brickset/2-set_exploration.R
+++ lego-pricing/brickset/2-set_exploration.R
@@ -1,4 +1,5 @@
 # let's explore the set data some, shall we?
+library(ggplot2)
 
 brickSets$legoID <- paste0(brickSets$number, "-", brickSets$numberVariant)
 # how many sets have prices?
@@ -24,6 +25,14 @@ nrow(bothPriced) / nrow(peeronSetsPriceIds)
 # brickset is still much better to use, even though the pricing ratio is the same.
 nrow(bothPriced) - nrow(peeronSetsPriceIds)
 # losing 223 sets, not bad
-
+rm(brickSetsPriceIds, peeronSetsPriceIds, bothPriced)
 # explore the brickSet set list
 tapply(brickSetsPriced$USRetailPrice, brickSetsPriced$year, mean)
+# scatter the year, price, number of pieces, number owned, number wanted, number of reviews, rating
+ggplot(brickSetsPriced, aes(x = year, y = USRetailPrice)) +
+  geom_point(shape = 1)
+ggplot(brickSetsPriced, aes(x = year, y = pieces)) +
+  geom_point(shape = 1)
+cor(brickSetsPriced$USRetailPrice, brickSetsPriced$pieces, use = "complete.obs")
+ggplot(brickSetsPriced, aes(x = USRetailPrice, y = pieces)) +
+  geom_point(shape = 1)
